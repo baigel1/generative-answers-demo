9677f788861c6bbb6cc41c040e84f90c999403c7
Daniel Baigel <dbaigel@yext.com>
2024-07-30 10:49:15 -0400

    styled cards

--- src/components/SearchResultDisplay.tsx
+++ src/components/SearchResultDisplay.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import SearchResultCard from './SearchResultCard';
+import * as React from "react";
+import SearchResultCard from "./SearchResultCard";
 
 const SearchResultsDisplay = ({ results }) => {
   return (
