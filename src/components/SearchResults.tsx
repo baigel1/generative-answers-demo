ccf3b4f2fdb990dce4cce3f3d9fb51d1a94f2ba1
Daniel Baigel <dbaigel@yext.com>
2024-07-30 11:19:34 -0400

    removed consoles

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,22 +1,19 @@
 import * as React from "react";
 
-import { VerticalResults } from "@yext/search-ui-react";
 import { useSearchState } from "@yext/search-headless-react";
 
-import BookCard from "./BookCard";
 import GenerativeAnswer from "./GenerativeAnswer";
 import SearchResultDisplay from "./SearchResultDisplay";
 
 const SearchResults = () => {
-  console.log("search results are");
   const verticalResults = useSearchState((state) => state.vertical.results);
-  console.log(verticalResults);
 
   return (
     <>
       <GenerativeAnswer />
-      {verticalResults && verticalResults.length > 0 &&
-        <SearchResultDisplay results={verticalResults} />}
+      {verticalResults && verticalResults.length > 0 && (
+        <SearchResultDisplay results={verticalResults} />
+      )}
     </>
   );
 };
