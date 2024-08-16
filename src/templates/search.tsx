7995303a040be36dcf88a1af87eecf5cc77b319d
Deepak Srinivasan <dsrinivasan@yext.com>
2024-08-15 22:43:05 -0400

    Update Page Title

--- src/templates/search.tsx
+++ src/templates/search.tsx
@@ -20,7 +20,7 @@ export const getHeadConfig: GetHeadConfig<
   TemplateRenderProps
 > = (): HeadConfig => {
   return {
-    title: `Harry Potter Search`,
+    title: `React SDK Harry Potter Search`,
     charset: "UTF-8",
     viewport: "width=device-width, initial-scale=1",
   };
